bd769234d0c884c4ff0255bed610fa5aa3df9231
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2019-05-06 19:50:16 -0300

    Added new SHA-1 tests

--- Tools/SHA-1/sha 1/driver.c
+++ Tools/SHA-1/sha 1/driver.c
@@ -29,20 +29,39 @@
 #define TEST4b  "01234567012345670123456701234567"
 /* an exact multiple of 512 bits */
 #define TEST4   TEST4a TEST4b
-char* testarray[4] =
+#define TEST5 ""
+#define TEST6 "message digest"
+#define TEST7 "abcdefghijklmnopqrstuvwxyz"
+#define TEST8 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+#define TEST9 "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl"
+#define TEST10 "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+
+char* testarray[10] =
     {
         TEST1,
         TEST2,
         TEST3,
-        TEST4
+        TEST4,
+        TEST5,
+        TEST6,
+        TEST7,
+        TEST8,
+        TEST9,
+        TEST10,
     };
-long int repeatcount[4] = {1, 1, 1000000, 10};
-char* resultarray[4] =
+long int repeatcount[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+char* resultarray[10] =
     {
         "A9 99 3E 36 47 06 81 6A BA 3E 25 71 78 50 C2 6C 9C D0 D8 9D",
         "84 98 3E 44 1C 3B D2 6E BA AE 4A A1 F9 51 29 E5 E5 46 70 F1",
-        "34 AA 97 3C D4 C4 DA A4 F6 1E EB 2B DB AD 27 31 65 34 01 6F",
-        "DE A3 56 A2 CD DD 90 C7 A7 EC ED C5 EB B5 63 93 4F 46 04 52"
+        "86 F7 E4 37 FA A5 A7 FC E1 5D 1D DC B9 EA EA EA 37 76 67 B8",
+        "E0 C0 94 E8 67 EF 46 C3 50 EF 54 A7 F5 9D D6 0B ED 92 AE 83",
+        "DA 39 A3 EE 5E 6B 4B 0D 32 55 BF EF 95 60 18 90 AF D8 07 09",
+        "C1 22 52 CE DA 8B E8 99 4D 5F A0 29 0A 47 23 1C 1D 16 AA E3",
+        "32 D1 0C 7B 8C F9 65 70 CA 04 CE 37 F2 A1 9D 84 24 0D 3A 89",
+        "76 1C 45 7B F7 3B 14 D2 7E 9E 92 65 C4 6F 4B 4D DA 11 F9 40",
+        "93 24 9D 4C 2F 89 03 EB F4 1A C3 58 47 31 48 AE 6D DD 70 42",
+        "50 AB F5 70 6A 15 09 90 A0 8B 2C 5E A4 0F A0 E5 85 55 47 32",
     };
 
 
@@ -54,7 +73,7 @@ int main() {
     /*
      *  Perform SHA-1 tests
      */
-    for (j = 0; j < 4; ++j) {
+    for (j = 0; j < 10; ++j) {
         printf("\nTest %d: %d, '%s'\n",
                j + 1,
                repeatcount[j],
