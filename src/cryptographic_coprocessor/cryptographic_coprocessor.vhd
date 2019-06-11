fa2efa449e3134a2db83b0d6202dba01bdf67a09
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2019-06-11 18:20:14 -0300

    Improved coprocessor

--- src/cryptographic_coprocessor/cryptographic_coprocessor.vhd
+++ src/cryptographic_coprocessor/cryptographic_coprocessor.vhd
@@ -9,8 +9,9 @@ entity cryptographic_coprocessor is
 	port(
 		clk         : in  std_logic;
 		instruction : in  std_logic_vector(31 downto 0);
-		r1, r2      : in  unsigned(31 downto 0);
-		output      : out std_logic_vector(31 downto 0)
+		data_in     : in  unsigned(31 downto 0);
+		r2          : in  unsigned(31 downto 0);
+		output      : out unsigned(31 downto 0)
 	);
 end entity;
 
@@ -114,16 +115,93 @@ begin
 	sha256_calculate_next_block <= is_sha256 and (is_next or is_last);
 	sha512_calculate_next_block <= is_sha512 and (is_next or is_last);
 
+	-- Update output
+	process(funct3, funct2, r2, md5_A_out, md5_B_out, md5_C_out, md5_D_out, md5_is_complete, sha1_H0_out, sha1_H1_out, sha1_H2_out, sha1_H3_out, sha1_H4_out, sha1_is_complete, sha256_H0_out, sha256_H1_out, sha256_H2_out, sha256_H3_out, sha256_H4_out, sha256_H5_out, sha256_H6_out, sha256_H7_out, sha256_is_complete, sha512_H0_out, sha512_H1_out, sha512_H2_out, sha512_H3_out, sha512_H4_out, sha512_H5_out, sha512_H6_out, sha512_H7_out, sha512_is_complete) is
+	begin
+		case funct3 is
+			when FUNCT3_COMPLETED =>
+				output(31 downto 1) <= (others => '0');
+				case funct2 is
+					when FUNCT2_MD5    => output(0) <= md5_is_complete;
+					when FUNCT2_SHA1   => output(0) <= sha1_is_complete;
+					when FUNCT2_SHA256 => output(0) <= sha256_is_complete;
+					when FUNCT2_SHA512 => output(0) <= sha512_is_complete;
+					when others        => output(0) <= '0'; -- TODO: ERROR
+				end case;
+
+			when FUNCT3_DIGEST =>
+				case funct2 is
+					when FUNCT2_MD5 =>
+						case to_integer(r2) is
+							when 0      => output <= md5_A_out;
+							when 1      => output <= md5_B_out;
+							when 2      => output <= md5_C_out;
+							when 3      => output <= md5_D_out;
+							when others => output <= (others => '0'); -- TODO: ERROR
+						end case;
+
+					when FUNCT2_SHA1 =>
+						case to_integer(r2) is
+							when 0      => output <= sha1_H0_out;
+							when 1      => output <= sha1_H1_out;
+							when 2      => output <= sha1_H2_out;
+							when 3      => output <= sha1_H3_out;
+							when 4      => output <= sha1_H4_out;
+							when others => output <= (others => '0'); -- TODO: ERROR
+						end case;
+
+					when FUNCT2_SHA256 =>
+						case to_integer(r2) is
+							when 0      => output <= sha256_H0_out;
+							when 1      => output <= sha256_H1_out;
+							when 2      => output <= sha256_H2_out;
+							when 3      => output <= sha256_H3_out;
+							when 4      => output <= sha256_H4_out;
+							when 5      => output <= sha256_H5_out;
+							when 6      => output <= sha256_H6_out;
+							when 7      => output <= sha256_H7_out;
+							when others => output <= (others => '0'); -- TODO: ERROR
+						end case;
+
+					when FUNCT2_SHA512 =>
+						case to_integer(r2) is
+							when 0      => output <= sha512_H0_out(63 downto 32);
+							when 1      => output <= sha512_H0_out(31 downto 0);
+							when 2      => output <= sha512_H1_out(63 downto 32);
+							when 3      => output <= sha512_H1_out(31 downto 0);
+							when 4      => output <= sha512_H2_out(63 downto 32);
+							when 5      => output <= sha512_H2_out(31 downto 0);
+							when 6      => output <= sha512_H3_out(63 downto 32);
+							when 7      => output <= sha512_H3_out(31 downto 0);
+							when 8      => output <= sha512_H4_out(63 downto 32);
+							when 9      => output <= sha512_H4_out(31 downto 0);
+							when 10     => output <= sha512_H5_out(63 downto 32);
+							when 11     => output <= sha512_H5_out(31 downto 0);
+							when 12     => output <= sha512_H6_out(63 downto 32);
+							when 13     => output <= sha512_H6_out(31 downto 0);
+							when 14     => output <= sha512_H7_out(63 downto 32);
+							when 15     => output <= sha512_H7_out(31 downto 0);
+							when others => output <= (others => '0'); -- TODO: ERROR
+						end case;
+
+					when others => output(0) <= '0'; -- TODO: ERROR
+				end case;
+
+			when others =>
+				output <= (others => '0');
+		end case;
+	end process;
+
 	md5 : entity work.md5
 		port map(
 			clk                   => clk,
 			start_new_hash        => md5_start_new_hash,
 			write_data_in         => md5_write_data_in,
-			data_in               => r1,
+			data_in               => data_in,
 			data_in_word_position => r2(3 downto 0),
 			calculate_next_block  => md5_calculate_next_block,
 			is_last_block         => is_last,
-			last_block_size       => r1(9 downto 0),
+			last_block_size       => r2(9 downto 0),
 			is_waiting_next_block => md5_is_waiting_next_block,
 			is_busy               => md5_is_busy,
 			is_complete           => md5_is_complete,
@@ -139,11 +217,11 @@ begin
 			clk                   => clk,
 			start_new_hash        => sha1_start_new_hash,
 			write_data_in         => sha1_write_data_in,
-			data_in               => r1,
+			data_in               => data_in,
 			data_in_word_position => r2(3 downto 0),
 			calculate_next_block  => sha1_calculate_next_block,
 			is_last_block         => is_last,
-			last_block_size       => r1(9 downto 0),
+			last_block_size       => r2(9 downto 0),
 			is_waiting_next_block => sha1_is_waiting_next_block,
 			is_busy               => sha1_is_busy,
 			is_complete           => sha1_is_complete,
@@ -160,11 +238,11 @@ begin
 			clk                   => clk,
 			start_new_hash        => sha256_start_new_hash,
 			write_data_in         => sha256_write_data_in,
-			data_in               => r1,
+			data_in               => data_in,
 			data_in_word_position => r2(3 downto 0),
 			calculate_next_block  => sha256_calculate_next_block,
 			is_last_block         => is_last,
-			last_block_size       => r1(9 downto 0),
+			last_block_size       => r2(9 downto 0),
 			is_waiting_next_block => sha256_is_waiting_next_block,
 			is_busy               => sha256_is_busy,
 			is_complete           => sha256_is_complete,
@@ -184,11 +262,11 @@ begin
 			clk                   => clk,
 			start_new_hash        => sha512_start_new_hash,
 			write_data_in         => sha512_write_data_in,
-			data_in               => r1,
+			data_in               => data_in,
 			data_in_word_position => r2(4 downto 0),
 			calculate_next_block  => sha512_calculate_next_block,
 			is_last_block         => is_last,
-			last_block_size       => r1(10 downto 0),
+			last_block_size       => r2(10 downto 0),
 			is_waiting_next_block => sha512_is_waiting_next_block,
 			is_busy               => sha512_is_busy,
 			is_complete           => sha512_is_complete,
@@ -202,26 +280,5 @@ begin
 			H6_out                => sha512_H6_out,
 			H7_out                => sha512_H7_out
 		);
-
-	-- Update output
-	process(funct3, funct2) is
-	begin
-		case funct3 is
-			when FUNCT3_COMPLETED =>
-				output(31 downto 1) <= (others => '0');
-				case funct2 is
-					when FUNCT2_MD5    => output(0) <= md5_is_complete;
-					when FUNCT2_SHA1   => output(0) <= sha1_is_complete;
-					when FUNCT2_SHA256 => output(0) <= sha256_is_complete;
-					when FUNCT2_SHA512 => output(0) <= sha512_is_complete;
-					when others        => output(0) <= '0';
-				end case;
-
-			when FUNCT3_DIGEST => null;
-
-			when others =>
-				output <= (others => '0');
-		end case;
-	end process;
 
 end architecture cryptographic_coprocessor_arch;
